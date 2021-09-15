caa5d5b335fc27d5726f996c0351c49dea504988
Mariano Bartolome <marianob@itcsoluciones.com>
2021-09-15 14:19:28 -0300

    Fin sección 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+
+
 
 
 
@@ -11,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
+ 
   ],
   exports: [
     BreadcrumbsComponent,
@@ -18,7 +23,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
