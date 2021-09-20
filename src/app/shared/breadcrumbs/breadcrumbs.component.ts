7238056262050c917b56162910363175fd5b4c01
Mariano Bartolome <marianob@itcsoluciones.com>
2021-09-20 09:17:27 -0300

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router, Event, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy  {
 
-  constructor() { }
+  public titulo: string | undefined;
+  public tituloSubs$: Subscription | undefined;
 
-  ngOnInit(): void {
+  constructor( private router: Router, private route: ActivatedRoute) { 
+    //console.log(route.snapshot.data);
+    this.tituloSubs$ = this.getArgumentosRuta()
+                        .subscribe( ({ titulo })  => {
+                          this.titulo=titulo;
+                          document.title=`AdminPro - ${ titulo }`;
+                        });
   }
+  ngOnDestroy(): void {
+    this.tituloSubs$?.unsubscribe();
+  }
+
+  getArgumentosRuta(){
+
+    return this.router.events
+      .pipe(
+        filter( (event: Event ): event is ActivationEnd => event instanceof ActivationEnd ), // linea reemplazada por la del curso, importando Event del @angular/router
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data)
+      );
+      
+
+  }
+
 
 }
