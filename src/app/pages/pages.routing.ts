8ada57e9c0d19149ce977be6b21bd3240e47ed86
mariano <marianob@itcsoluciones.com>
2022-03-02 17:30:23 -0300

    Enviroment produccion listo

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -3,20 +3,7 @@ import { NgModule } from '@angular/core';
 import { AuthGuard } from '../guards/auth.guard';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
 
-//Mantenimientos
-import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
-import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
-import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-import { MedicoComponent } from './mantenimientos/medicos/medico.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
 
 
 const routes: Routes = [
@@ -24,22 +11,8 @@ const routes: Routes = [
         path: 'dashboard', 
         component: PagesComponent,
         canActivate: [AuthGuard],
-        children: [
-            { path: '', component: DashboardComponent, data: {titulo:'Dashboard'} },
-            { path: 'account-settings', component: AccountSettingsComponent, data: {titulo:'Ajustes de cuenta'} },
-            { path: 'buscar/:termino', component: BusquedaComponent, data: {titulo:'Búsquedas'} },
-            { path: 'grafica1', component: Grafica1Component, data: {titulo:'Gráfica #1'} },
-            { path: 'perfil', component: PerfilComponent, data: {titulo:'Perfil'} },
-            { path: 'progress', component: ProgressComponent, data: {titulo:'Barra de progreso'} },
-            { path: 'promesas', component: PromesasComponent, data: {titulo:'Promesas'} },
-            { path: 'rxjs', component: RxjsComponent, data: {titulo:'Rxjs'} },
-
-            //Mantenimientos
-            { path: 'hospitales', component: HospitalesComponent, data: {titulo:'Mantenimiento de hospitales'} },
-            { path: 'medicos', component: MedicosComponent, data: {titulo:'Mantenimiento de médicos'} },
-            { path: 'medico/:id', component: MedicoComponent, data: {titulo:'Mantenimiento de médicos'} },
-            { path: 'usuarios', component: UsuariosComponent, data: {titulo:'Mantenimiento de usuarios'} },
-        ]
+        canLoad:[ AuthGuard ],
+        loadChildren:()=> import('./child-routes.module').then(m=>m.ChildRoutesModule)
     },
 ];
 
